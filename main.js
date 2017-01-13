69d01fbb6e1dd20026d56fbf55d66fe6e6339636
Kukrim Choe <choeku86@gmail.com>
2017-01-12 21:25:27 -0800

    Ajax call, able to list names and images, up until the player has 6 pokemon in hand.

--- main.js
+++ main.js
@@ -11,26 +11,51 @@ function playerConstructor(name){
     player.hand.push(card);
   };
   return player;
-};
-
- function getRandomNumber(){
-  return Math.floor(Math.random() * (151 - 0 + 1)); //give us random card number
 }
 
+function getRandomNumber(){
+  return Math.floor(Math.random() * 151); //give us random card number
+}
+
+var hand = [];
+var imgAddress = [];
+var html_str = "";
+var img_str = "";
+
 $(document).ready(function(){  //button to generate random card
-  $.get('http://pokeapi.co/api/v2/pokemon/' + getRandomNumber(), function(pokemon){
-    console.log("1: ", pokemon.name);
-  });
-
   $("#submit").click(function() {
-    var pokemon = $.ajax("http://pokeapi.co/api/v2/pokemon/" + getRandomNumber());
-    function(pokemon){
-      console.log("1: ", pokemon.name);
-    });
-    var card = 'http://pokeapi.co/media/img/' + getRandomNumber() + '.png';
+    var id = getRandomNumber();
+    // if (html_str == null) {
+    //     img_str = "<img src = 'http://pokeapi.co/media/img/" + id + ".png' >";
+    //     html_str = "<h5> http://pokeapi.co/api/v2/pokemon/" + id + "</h5>";
+    // }
+    var pokemon = $.ajax({url: "http://pokeapi.co/api/v2/pokemon/" + id, success: function(pokemon){
+      hand.push(pokemon.name);
+      
+      if (hand.length < 7) {
+        img_str += "<img src = 'http://pokeapi.co/media/img/" + id + ".png' >";
+        html_str = "";
+        html_str += "<div> <h5>";
+        for (var i=0; i<hand.length; i++) {
+          if(i == 0) {
+            html_str += hand[i];
+          } else {
+            html_str += ", " + hand[i];
+          }
+        }
+        html_str += "</h5> </div>";
 
-    console.log(pokemon);
-    console.log("Hi There!");
+        console.log(hand);
+
+      } else {
+        html_str_fullHand = "<h1> You have a full hand </h1>"
+        $('#warning').html(html_str_fullHand);
+      }
+    }});
+    
+    $('#info').html(html_str);
+    $('#img').html(img_str);
+    var card = 'http://pokeapi.co/media/img/' + id + '.png';
+    imgAddress.push(card);
   });
-
 })
